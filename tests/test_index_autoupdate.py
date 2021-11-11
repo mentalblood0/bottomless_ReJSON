ac4c80d7b8ac5077edca5424fa48b85c6bf1fd08
necheporenko_s_iu <neceporenkostepan@gmail.com>
2021-11-11 15:33:45 +0300

    added test_add_simple_after_complex

--- tests/test_index_autoupdate.py
+++ tests/test_index_autoupdate.py
@@ -41,3 +41,29 @@ def test_add_complex():
 		interface['sessions']['a'],
 		interface['sessions']['c']
 	]
+
+
+def test_add_simple_after_complex():
+
+	interface = RedisInterface(host=config['db']['host'], port=config['db']['port'])
+	interface.clear()
+	interface.indexes.clear()
+
+	interface['sessions'].createIndex('state')
+
+	interface['sessions'] = {
+		'a': {'state': 'new'},
+		'b': {'state': 'processed'},
+		'c': {'state': 'new'},
+		'd': {'state': 'processed'},
+		'e': {'state': 'erroneous'}
+	}
+
+	interface['sessions']['f'] = {'state': 'new'}
+
+	assert interface.indexes['sessions']['__index__']['state']['new']['f']() == True
+	assert interface['sessions'].filter('state', 'new') == [
+		interface['sessions']['a'],
+		interface['sessions']['c'],
+		interface['sessions']['f']
+	]
