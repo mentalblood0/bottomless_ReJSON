98fb9a7568863bd0347eb45588004dd580fa99f3
necheporenko_s_iu <neceporenkostepan@gmail.com>
2021-11-18 17:44:36 +0300

    added benchmark_clear

--- benchmarks/benchmark_clear.py
+++ benchmarks/benchmark_clear.py
@@ -5,7 +5,7 @@ import bottomless_ReJSON.RedisInterface as RedisInterface
 
 
 
-class same(Benchmark):
+class without_index(Benchmark):
 
 	def prepare(self, items_number):
 
@@ -19,28 +19,7 @@ class same(Benchmark):
 		}
 
 	def run(self, **kwargs):
-		self.interface['sessions'].createIndex('state')
-	
-	def clean(self, **kwargs):
-		self.interface.indexes.clear()
 		self.interface.clear()
-
-
-class different(Benchmark):
-
-	def prepare(self, items_number):
-
-		self.interface = RedisInterface(host=config['db']['host'], port=config['db']['port'])
-		self.interface.indexes.clear()
-		self.interface.clear()
-
-		self.interface['sessions'] = {
-			str(i): {'state': str(i)}
-			for i in range(items_number)
-		}
-
-	def run(self, **kwargs):
-		self.interface['sessions'].createIndex('state')
 	
 	def clean(self, **kwargs):
 		self.interface.indexes.clear()
