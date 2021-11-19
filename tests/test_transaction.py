ade8c085ef6e70a09d65765ca08ba9725c52b469
necheporenko_s_iu <neceporenkostepan@gmail.com>
2021-11-19 11:07:32 +0300

    added test_interrupt for transactions

---
+++ tests/test_transaction.py
@@ -0,0 +1,28 @@
+import uuid
+import pytest
+
+from tests import config
+import bottomless_ReJSON.RedisInterface as RedisInterface
+
+
+
+def test_interrupt():
+
+	interface = RedisInterface(host=config['db']['host'], port=config['db']['port'])
+	interface.indexes.clear()
+	interface.clear()
+
+	db = interface.db
+
+	key = 'default'
+	value = uuid.uuid4().hex
+
+	def transaction_function(pipe):
+
+		if pipe.get(key) != value:
+			pipe.set(key, value)
+		
+		pipe.multi()
+		pipe.jsonset('root', '.', {'a': 1})
+	
+	db.transaction(transaction_function, key)
