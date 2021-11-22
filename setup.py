e88d8e08d49216787c67fcfc0bf60402fdbbd5be
necheporenko_s_iu <neceporenkostepan@gmail.com>
2021-11-22 14:32:32 +0300

    changed version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 
 	setup(
 		name='bottomless_ReJSON',
-		version='0.6',
+		version='0.7',
 		description='Correct Redis Library',
 		long_description=long_description,
 		long_description_content_type='text/markdown',
